4777e6df337a9208c0470f723deb4033b93e7ad3
toshimichi yokota <y.t.38852118@icloud.com>
2019-12-10 12:45:14 +0000

    post

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -11,9 +11,10 @@
                 </div>
                 <div>
                     @if (Auth::id() == $micropost->user_id)
-                        {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' = > 'delete']) !!}
+                        {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                             {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
                         {!! Form::close() !!}
+                    @endif    
                 </div>
             </div>
         </li>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -17,8 +17,8 @@
                 <li class="nav-item"><a href="{{ route('users.show', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/' . $user->id) ? 'active' : '' }}">TimeLine <span class="badge badge-secondary">{{ $count_microposts }}</span></a></li>
                 <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
-            </ul>]
-        @if (Auth:id() == $user->id)
+            </ul>
+        @if (Auth::id() == $user->id)
             {!! Form::open(['route' => 'microposts.store']) !!}
                 <div class="form-group">
                     {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
@@ -26,8 +26,8 @@
                 </div>
             {!! Form::close() !!}
         @endif
-        @if (count($micropost) > 0)
-            @includ('micropost.micropost', ['micropsts' => $microposts])
+        @if (count($microposts) > 0)
+            @include('microposts.microposts', ['micropsts' => $microposts])
         @endif
         </div>
     </div>
